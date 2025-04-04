907894bdb89f4f67db4b603d42e451fa082454ae
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-04 23:13:13 +0200

    refactor: install sarama and update lambda function

--- main.go
+++ main.go
@@ -2,34 +2,72 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"crypto/tls"
 	"log"
-	"net/url"
+	"os"
 
-	"github.com/aws/aws-lambda-go/events"
+	"github.com/IBM/sarama"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// KafkaProducer function to send messages to Kafka
-func KafkaProducer(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// Parse form data from the request body
-	parsedFormData, err := url.ParseQuery(request.Body)
-	if err != nil {
-		log.Printf("Error parsing form data: %s", err)
-		return events.APIGatewayProxyResponse{StatusCode: 400}, err
+type Event struct {
+	Topic string `json:"topic"`
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func saramaConfig() *sarama.Config {
+	config := sarama.NewConfig()
+
+	// Kafka version
+	config.Version = sarama.V3_9_0_0 // Adjust according to your Kafka version
+
+	// SASL/SCRAM Authentication
+	config.Net.SASL.Enable = true
+	config.Net.SASL.Handshake = true
+	config.Net.SASL.User = os.Getenv("KAFKA_USERNAME")
+	config.Net.SASL.Password = os.Getenv("KAFKA_PASSWORD")
+	config.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA256
+
+	// TLS (optional but recommended)
+	config.Net.TLS.Enable = true
+	config.Net.TLS.Config = &tls.Config{
+		InsecureSkipVerify: true, // Change to false in production
 	}
 
-	// Access POST parameters
-	key1 := parsedFormData.Get("key1")
-	key2 := parsedFormData.Get("key2")
+	// Producer settings
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
 
-	log.Printf("Received Key1: %s", key1)
-	log.Printf("Received Key2: %s", key2)
+	return config
+}
 
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       fmt.Sprintf(`%s %s`, key1, key2),
-	}, nil
+// KafkaProducer function to send messages to Kafka
+func KafkaProducer(ctx context.Context, event Event) error {
+	kafkaBrokers := []string{os.Getenv("KAFKA_BROKER")}
+	config := saramaConfig()
+	producer, err := sarama.NewSyncProducer(kafkaBrokers, config)
+	if err != nil {
+		log.Printf("Failed to start Sarama producer: %v", err)
+		return err
+	}
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: event.Topic,
+		Key:   sarama.StringEncoder(event.Key),
+		Value: sarama.StringEncoder(event.Value),
+	}
+
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("Failed to send message: %v", err)
+		return err
+	}
+
+	log.Printf("Message sent to partition %d at offset %d", partition, offset)
+	return nil
 }
 
 func main() {
